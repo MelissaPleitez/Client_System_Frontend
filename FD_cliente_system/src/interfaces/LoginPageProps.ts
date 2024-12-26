ef29c00e28e7748f44ab2c549e888d035ebc128c
MelissaPleitez <melissa.ramirez0pleitez@gmail.com>
2024-12-26 10:38:31 -0600

    Adding client and user store using mobx and creating components and interfaces

---
+++ FD_cliente_system/src/interfaces/LoginPageProps.ts
@@ -0,0 +1,11 @@
+export interface LoginPageProps {
+    type: string;
+    placeholder: string;
+    icon: string;
+    onChange: any;
+    autocomplete: any;
+}
+
+export interface LoginErroMessage {
+    error: string;
+}
