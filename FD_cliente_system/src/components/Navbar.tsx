0b669dc873c471ba4a3851c067f8c4af98e7aded
MelissaPleitez <melissa.ramirez0pleitez@gmail.com>
2024-12-29 22:12:56 -0600

    Adding logo and css and updating table logic

--- FD_cliente_system/src/components/Navbar.tsx
+++ FD_cliente_system/src/components/Navbar.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "../assets/css/mainPage.css";
 import {LogOut, Table} from "react-feather";
+import imagen from '../assets/imgs/logo.png';
 
 const Navbar: React.FC = () => {
   return (
     <nav className="navbar">
       <div className="navbar_logo"><Table color='#ffffff' size="24" /></div>
+      <img 
+          src={imagen}
+          alt="Icono" 
+          className="navbar_icon" 
+          style={{ width: '200px', height: '50px', margin: '0 8px' }} 
+        />
       <ul className="navbar_links">
         <li><a href="/">LogOut  <LogOut color='#ffffff' size="24" /> </a></li>
       </ul>
